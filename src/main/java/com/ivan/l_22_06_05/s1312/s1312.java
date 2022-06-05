86d78b678e538bbfd4aa80d362e0eaa2b434dda9
ivan <ivanli117.com>
2022-06-05 11:48:21 +0800

    让字符串成为回文串的最小插入次数

--- src/main/java/com/ivan/l_22_06_05/s1312/s1312.java
+++ src/main/java/com/ivan/l_22_06_05/s1312/s1312.java
@@ -1,4 +1,28 @@
 package com.ivan.l_22_06_05.s1312;
 
+import java.util.Arrays;
+
 public class s1312 {
+    public int minInsertions(String s) {
+        // dp
+        int[][] dp = new int[s.length()][s.length()];
+
+        // base case
+        for (int i = 0; i < s.length(); i++) {
+            Arrays.fill(dp[i], 0);
+        }
+
+        // cases and choices
+        for (int i = s.length() - 2; i >= 0; i--) {
+            for (int j = i + 1; j < s.length(); j++) {
+                if (s.charAt(i) == s.charAt(j)) {
+                    dp[i][j] = dp[i + 1][j - 1];
+                } else {
+                    dp[i][j] = Math.min(dp[i][j - 1], dp[i + 1][j]) + 1;
+                }
+            }
+        }
+
+        return dp[0][s.length() - 1];
+    }
 }
---
+++ src/test/java/com/ivan/l_22_06_05/s1312/s1312Test.java
@@ -0,0 +1,12 @@
+package com.ivan.l_22_06_05.s1312;
+
+import junit.framework.TestCase;
+
+public class s1312Test extends TestCase {
+    private s1312 s1312 = new s1312();
+
+    public void test() {
+        int res = s1312.minInsertions("leetcode");
+        System.out.println("res = " + res);
+    }
+}
