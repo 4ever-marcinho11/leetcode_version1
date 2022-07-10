966caace999d33a71f7e5c2e66cf1b8404695674
ivan <ivanli117.com>
2022-07-10 11:30:06 +0800

    添加相关注释

--- src/main/java/com/ivan/l_22_07_10/s124/s124.java
+++ src/main/java/com/ivan/l_22_07_10/s124/s124.java
@@ -16,11 +16,15 @@ public class s124 {
         int left = findMax(root.left);
         int right = findMax(root.right);
 
+        // 不联络root的父节点或本身是根节点
         int all = root.val + Math.max(0, left) + Math.max(0, right);
+        // 联络父节点
         int part = root.val + Math.max(0, Math.max(left, right));
 
+        // 更新最大值
         finalAns = Math.max(finalAns, Math.max(all, part));
 
+        // 返回能联络的
         return part;
     }
 
