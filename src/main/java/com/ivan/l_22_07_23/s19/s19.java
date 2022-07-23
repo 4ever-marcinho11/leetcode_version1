6e748ec83c686299875ac1f084d61f0693bbc03b
ivan <ivanli117.com>
2022-07-23 20:24:04 +0800

    删除链表的倒数第k个节点

---
+++ src/main/java/com/ivan/l_22_07_23/s19/s19.java
@@ -0,0 +1,45 @@
+package com.ivan.l_22_07_23.s19;
+
+public class s19 {
+    public ListNode removeNthFromEnd(ListNode head, int n) {
+        ListNode front = new ListNode(-1);
+        front.next = head;
+        ListNode ptr = front;
+
+        // 计算长度
+        int length = 0;
+        while (ptr != null) {
+            length++;
+            ptr = ptr.next;
+        }
+
+        // 找到倒数第n+1个
+        ptr = front;
+        int range = length - n - 1;
+        while (range > 0) {
+            ptr = ptr.next;
+            range--;
+        }
+        ptr.next = ptr.next.next;
+
+        return front.next;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode() {
+    }
+
+    ListNode(int val) {
+        this.val = val;
+    }
+
+    ListNode(int val, ListNode next) {
+        this.val = val;
+        this.next = next;
+    }
+}
+
