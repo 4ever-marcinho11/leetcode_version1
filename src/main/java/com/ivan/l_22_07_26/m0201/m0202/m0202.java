fb26fb985a4d8d023162b7ff315773581123be6a
ivan <ivanli117.com>
2022-07-26 21:03:09 +0800

    返回链表倒数第k个节点

---
+++ src/main/java/com/ivan/l_22_07_26/m0201/m0202/m0202.java
@@ -0,0 +1,32 @@
+package com.ivan.l_22_07_26.m0201.m0202;
+
+public class m0202 {
+    public int kthToLast(ListNode head, int k) {
+        ListNode fast = head, slow = head;
+
+        if (head == null) {
+            return -1;
+        }
+
+        while (k > 0) {
+            fast = fast.next;
+            k--;
+        }
+
+        while (fast != null) {
+            slow = slow.next;
+            fast = fast.next;
+        }
+
+        return slow.val;
+    }
+}
+
+class ListNode {
+    int val;
+    ListNode next;
+
+    ListNode(int x) {
+        val = x;
+    }
+}
