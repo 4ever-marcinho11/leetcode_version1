efd9dcc45b8a39eccd5be839e1c4712d4eccba74
ivan <ivanli117.com>
2022-08-09 21:37:52 +0800

    螺旋矩阵2

---
+++ src/main/java/com/ivan/l_22_08_09/s59/s59.java
@@ -0,0 +1,47 @@
+package com.ivan.l_22_08_09.s59;
+
+/**
+ * @author Maximilian_Li
+ */
+public class s59 {
+    public int[][] generateMatrix(int n) {
+        // 第几圈，控制循环次数
+        int loop = 0;
+        int[][] res = new int[n][n];
+        // 每次循环的开始点(start, start)
+        int start = 0;
+        // 定义填充数字
+        int count = 1;
+        int i, j;
+
+        // 判断边界后，loop从1开始
+        while (loop++ < n / 2) {
+            // 模拟上侧从左到右
+            for (j = start; j < n - loop; j++) {
+                res[start][j] = count++;
+            }
+
+            // 模拟右侧从上到下
+            for (i = start; i < n - loop; i++) {
+                res[i][j] = count++;
+            }
+
+            // 模拟下侧从右到左
+            for (; j >= loop; j--) {
+                res[i][j] = count++;
+            }
+
+            // 模拟左侧从下到上
+            for (; i >= loop; i--) {
+                res[i][j] = count++;
+            }
+            start++;
+        }
+
+        if (n % 2 == 1) {
+            res[start][start] = count;
+        }
+
+        return res;
+    }
+}
