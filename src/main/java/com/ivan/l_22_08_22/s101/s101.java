3324219d6d11f6ff5688160d40c2724af2611172
ivan <ivanli117.com>
2022-08-22 21:32:56 +0800

    对称二叉树

---
+++ src/main/java/com/ivan/l_22_08_22/s101/s101.java
@@ -0,0 +1,61 @@
+package com.ivan.l_22_08_22.s101;
+
+/**
+ * @author Maximilian_Li
+ */
+public class s101 {
+    public boolean isSymmetric(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+
+        return compare(root.left, root.right);
+    }
+
+    /**
+     * 判断root的左右节点是否相同
+     *
+     * @param left  左节点
+     * @param right 右节点
+     * @return boolean
+     */
+    private boolean compare(TreeNode left, TreeNode right) {
+        if (left != null && right == null) {
+            // 左不空，右空
+            return false;
+        } else if (left == null && right != null) {
+            // 右不空，左空
+            return false;
+        } else if (left == null && right == null) {
+            // 都空
+            return true;
+        } else if (left.val != right.val) {
+            // 左右值不同
+            return false;
+        }
+
+        // 左右值相同的时候需要进行递归，判断下一层
+        boolean outer = compare(left.left, right.right);
+        boolean inner = compare(left.right, right.left);
+        return outer && inner;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
