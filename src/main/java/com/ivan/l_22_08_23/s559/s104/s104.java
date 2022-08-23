2c576fd9de47488057140e8e13f92b655f18c451
ivan <ivanli117.com>
2022-08-23 17:29:10 +0800

    二叉树的最大深度

---
+++ src/main/java/com/ivan/l_22_08_23/s559/s104/s104.java
@@ -0,0 +1,41 @@
+package com.ivan.l_22_08_23.s559.s104;
+
+
+/**
+ * @author Maximilian_Li
+ */
+public class s104 {
+    public int maxDepth(TreeNode root) {
+        return getDepth(root);
+    }
+
+    public int getDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+
+        int left = getDepth(root.left);
+        int right = getDepth(root.right);
+
+        return Integer.max(left, right) + 1;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
