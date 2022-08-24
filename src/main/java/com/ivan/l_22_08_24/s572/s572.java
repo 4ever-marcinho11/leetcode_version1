d85e8f780a8debef7b558b01905a6edb2e87eb2a
ivan <ivanli117.com>
2022-08-24 10:06:53 +0800

    另一棵树的子树

---
+++ src/main/java/com/ivan/l_22_08_24/s572/s572.java
@@ -0,0 +1,49 @@
+package com.ivan.l_22_08_24.s572;
+
+public class s572 {
+    public boolean isSubtree(TreeNode root, TreeNode subRoot) {
+        if (subRoot == null) {
+            return true;
+        }
+        if (root == null) {
+            return false;
+        }
+
+        return isSubtree(root, subRoot) || isSubtree(root.left, subRoot) || isSubtree(root.right, subRoot);
+    }
+
+    private boolean compare(TreeNode p, TreeNode q) {
+        if (p == null && q == null) {
+            return true;
+        } else if (p == null && q != null) {
+            return false;
+        } else if (p != null && q == null) {
+            return false;
+        } else if (p.val != q.val) {
+            return false;
+        }
+
+        boolean a = compare(p.left, q.left);
+        boolean b = compare(p.right, q.right);
+        return a && b;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
