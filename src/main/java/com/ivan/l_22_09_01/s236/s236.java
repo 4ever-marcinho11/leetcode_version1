0407cf3d5171d406ac8c331d366f20876d4a4f6d
ivan <ivanli117.com>
2022-09-01 09:51:34 +0800

    二叉树的最近公共祖先

---
+++ src/main/java/com/ivan/l_22_09_01/s236/s236.java
@@ -0,0 +1,44 @@
+package com.ivan.l_22_09_01.s236;
+
+/**
+ * @author Maximilian_Li
+ */
+public class s236 {
+    public TreeNode lowestCommonAncestor(TreeNode root, TreeNode p, TreeNode q) {
+        if (root == p || root == q || root == null) {
+            return root;
+        }
+
+        // 找两个节点的最近祖先可以从下往上遍历，选择后序遍历
+        TreeNode left = lowestCommonAncestor(root.left, p, q);
+        TreeNode right = lowestCommonAncestor(root.right, p, q);
+        if (left != null && right != null) {
+            return root;
+        } else if (left == null && right != null) {
+            return right;
+        } else if (left != null && right == null) {
+            return left;
+        } else {
+            return null;
+        }
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
