31413d67906467e1a1d4625fa254ca80f9743772
ivan <ivanli117.com>
2022-09-02 19:06:22 +0800

    二叉搜索树中的插入操作

---
+++ src/main/java/com/ivan/l_22_09_02/s701/s701.java
@@ -0,0 +1,39 @@
+package com.ivan.l_22_09_02.s701;
+
+/**
+ * @author Maximilian_Li
+ */
+public class s701 {
+    public TreeNode insertIntoBST(TreeNode root, int val) {
+        if (root == null) {
+            return new TreeNode(val);
+        }
+
+        if (root.val > val) {
+            root.left = insertIntoBST(root.left, val);
+        } else {
+            root.right = insertIntoBST(root.right, val);
+        }
+
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
