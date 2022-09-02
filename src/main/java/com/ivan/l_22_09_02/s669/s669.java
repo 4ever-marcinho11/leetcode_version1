90254a0d8c97465f1276cd21aaf0454ec82f1a9a
ivan <ivanli117.com>
2022-09-02 20:05:24 +0800

    修剪二叉搜索树

---
+++ src/main/java/com/ivan/l_22_09_02/s669/s669.java
@@ -0,0 +1,41 @@
+package com.ivan.l_22_09_02.s669;
+
+/**
+ * @author Maximilian_Li
+ */
+public class s669 {
+    public TreeNode trimBST(TreeNode root, int low, int high) {
+        if (root == null) {
+            return null;
+        }
+
+        if (root.val < low) {
+            return trimBST(root.right, low, high);
+        }
+        if (root.val > high) {
+            return trimBST(root.left, low, high);
+        }
+        root.left = trimBST(root.left, low, high);
+        root.right = trimBST(root.right, low, high);
+        return root;
+    }
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
