e42d19512ba4ee076e11db36196836e63b720e8d
ivan <ivanli117.com>
2022-09-03 21:47:44 +0800

    组合总和3

---
+++ src/main/java/com/ivan/l_22_09_03/s216/s216.java
@@ -0,0 +1,44 @@
+package com.ivan.l_22_09_03.s216;
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * @author Maximilian_Li
+ */
+public class s216 {
+    List<List<Integer>> res = new ArrayList<>();
+    LinkedList<Integer> path = new LinkedList<>();
+
+    /**
+     * 找出所有相加之和为 n 的 k 个数的组合，且满足下列条件：
+     * 只使用数字1到9
+     * 每个数字 最多使用一次
+     *
+     * @param k size
+     * @param n sum
+     * @return res
+     */
+    public List<List<Integer>> combinationSum3(int k, int n) {
+        bt(0, n, k, 1);
+        return res;
+    }
+
+    public void bt(int current, int target, int size, int start) {
+        if (path.size() == size) {
+            if (current == target) {
+                res.add(new ArrayList<>(path));
+            }
+            return;
+        }
+
+        for (int i = start; i <= 9; i++) {
+            path.add(i);
+            current += i;
+            bt(current, target, size, i + 1);
+            path.removeLast();
+            current -= i;
+        }
+    }
+}
