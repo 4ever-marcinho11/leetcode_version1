42adeb5a36cbb396fa50722f309b70ae9322e131
ivan <ivanli117.com>
2022-09-05 21:43:32 +0800

    复原IP地址

--- src/main/java/com/ivan/l_22_09_05/s93/s93.java
+++ src/main/java/com/ivan/l_22_09_05/s93/s93.java
@@ -11,59 +11,54 @@ import java.util.List;
  */
 public class s93 {
     List<String> res = new ArrayList<>();
+    StringBuilder path = new StringBuilder();
 
     public List<String> restoreIpAddresses(String s) {
-        if (s.length() > 12) {
-            return res;
-        }
-        bt(0, s, 0);
+
+        bt(s, 0, 0);
         return res;
     }
 
-    public void bt(int start, String s, int pointNum) {
-        if (pointNum == 3) {
-            // 如果第四段合法，就放入path中
-            if (isValid(s, start, s.length() - 1)) {
-                res.add(s);
-            }
+    /**
+     * 回溯
+     *
+     * @param s       字符串
+     * @param start   起始索引
+     * @param partNum 已有分段数目
+     */
+    public void bt(String s, int start, int partNum) {
+        if (s.length() == start && partNum == 4) {
+            // 遍历完 + 正好四段
+            res.add(path.toString());
             return;
         }
 
-        for (int i = start; i < s.length(); i++) {
-            if (isValid(s, start, i)) {
-                s = s.substring(0, i + 1) + "." + s.substring(i + 1);
-                pointNum++;
-            } else {
+        if (s.length() == start || partNum == 4) {
+            return;
+        }
+
+        for (
+                int i = start;
+                i < s.length() &&
+                        // 一段长度最大为3
+                        i - start < 3 &&
+                        // 0 <= x <= 255
+                        Integer.parseInt(s.substring(start, i + 1)) >= 0 &&
+                        Integer.parseInt(s.substring(start, i + 1)) <= 255;
+                i++
+        ) {
+            if (i - start > 0 && s.charAt(start) == '0') {
                 continue;
             }
-            bt(i + 2, s, pointNum);
-            s = s.substring(0, i + 1) + s.substring(i + 2);
-            pointNum--;
-        }
-    }
-
-    public boolean isValid(String s, int start, int end) {
-        if (start > end) {
-            return false;
-        }
-
-        if (s.charAt(start) == '0' && start != end) {
-            // 非0数字不能以0开头
-            return false;
-        }
-
-        int sum = 0;
-        for (int i = start; i <= end; i++) {
-            if (s.charAt(i) > '9' || s.charAt(i) < '0') {
-                // 不能包含非数字与.的字符
-                return false;
+            path.append(s, start, i + 1);
+            if (partNum < 3) {
+                // 已分三段已经有了三个.，就不用再加.l
+                path.append(".");
             }
-            sum = sum * 10 + Integer.parseInt(String.valueOf(s.charAt(i)));
-            if (sum > 255) {
-                // 一段字符一定在0-255的范围内
-                return false;
-            }
+            partNum++;
+            bt(s, i + 1, partNum);
+            path.delete(start + partNum, i + partNum + 2);
+            partNum--;
         }
-        return true;
     }
 }
