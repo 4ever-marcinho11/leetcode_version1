59f4081768fd5874931476b5eaa08a54debfa5c6
ivan <ivanli117.com>
2022-11-15 17:40:49 +0800

    单词拆分 + summary

--- src/main/java/com/ivan/l_22_11_15/summary_one/Summary.java
+++ src/main/java/com/ivan/l_22_11_15/summary_one/Summary.java
@@ -67,6 +67,10 @@ public class Summary {
             dp[j] = value[0];
         }
 
+        /*
+        如果求组合数就是外层for循环遍历物品，内层for遍历背包。
+        如果求排列数就是外层for遍历背包，内层for循环遍历物品。
+        */
         for (int i = 0; i < weight.length; i++) {
             for (int j = weight[i]; j <= bagsize; j++) {
                 dp[j] = Integer.max(dp[j], dp[j - weight[i]] + value[i]);
