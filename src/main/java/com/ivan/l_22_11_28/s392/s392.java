cfcbbd6514f8b6642c28ed2bf9d2b1c6aa73b655
ivan <1248420190@qq.com>
2022-11-28 09:25:29 +0800

    判断子序列

---
+++ src/main/java/com/ivan/l_22_11_28/s392/s392.java
@@ -0,0 +1,37 @@
+package com.ivan.l_22_11_28.s392;
+
+public class s392 {
+    /**
+     * 给定字符串 s 和 t ，判断 s 是否为 t 的子序列。
+     * <p>
+     * 字符串的一个子序列是原始字符串删除一些（也可以不删除）字符而不改变剩余字符相对位置形成的新字符串。
+     * （例如，"ace"是"abcde"的一个子序列，而"aec"不是）。
+     * <p>
+     * 进阶：
+     * <p>
+     * 如果有大量输入的 S，称作 S1, S2, ... , Sk 其中 k >= 10亿，你需要依次检查它们是否为 T 的子序列。
+     * 在这种情况下，你会怎样改变代码？
+     *
+     * @param s
+     * @param t
+     * @return
+     */
+    public boolean isSubsequence(String s, String t) {
+        int[][] dp = new int[s.length() + 1][t.length() + 1];
+
+        for (int i = 1; i <= s.length(); i++) {
+            for (int j = 1; j <= t.length(); j++) {
+                // 编辑距离入门
+                if (s.charAt(i - 1) == t.charAt(j - 1)) {
+                    dp[i][j] = dp[i - 1][j - 1] + 1;
+                } else {
+                    dp[i][j] = dp[i][j - 1];
+                }
+            }
+        }
+
+        System.out.println(dp[s.length()][t.length()]);
+
+        return s.length() == dp[s.length()][t.length()];
+    }
+}
